1626790c5529db1f4c852f3eec26da936914327c
Ahmed Hany <hanysayed189@gmail.com>
2025-11-19 02:22:23 +0200

    Add @vercel/node dependency to package.json for improved serverless function support.

---
+++ backend/api/index.ts
@@ -0,0 +1,8 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import app from "../src/app";
+
+// Express app أصلاً عبارة عن function (req, res, next),
+// فنقدر نناديه مباشرة
+export default (req: VercelRequest, res: VercelResponse) => {
+  return (app as any)(req, res);
+};
